534e8e484f536ed36af04f766125e515800c845f
chaoticunnatural <j.thomps1310@gmail.com>
2026-04-28 18:21:08 -0400

    silly

--- src/main/java/dev/wren/crowsnest/CrowsNestClient.java
+++ src/main/java/dev/wren/crowsnest/CrowsNestClient.java
@@ -1,12 +1,17 @@
 package dev.wren.crowsnest;
 
 import dev.wren.crowsnest.commands.CrowsNestCommands;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.client.event.RegisterClientCommandsEvent;
 import net.neoforged.neoforge.common.NeoForge;
 
-
+@Mod(value = CrowsNest.MODID, dist = Dist.CLIENT)
+@SuppressWarnings("unused")
 public class CrowsNestClient {
-    public static void init() {
+
+    public CrowsNestClient(IEventBus modEventBus) {
         NeoForge.EVENT_BUS.addListener(CrowsNestClient::registerClientCommands);
     }
 
